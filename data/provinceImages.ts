4bedc29b491876f57c8e59510e547ba30369a97f
Subin-mungmee <betapower00002@gmail.com>
2026-03-05 15:49:50 +0700

    2

--- data/provinceImages.ts
+++ data/provinceImages.ts
@@ -2,6 +2,8 @@ export const provinceImages: Record<string, string> = {
   "แม่ฮ่องสอน": "/imgnorth/maehongson/mae-hong-son.jpg",
   "นครสวรรค์": "/imgnorth/nakhon-sawan/nakhon-sawan.jpg",
   "พิจิตร": "/imgnorth/phichit/Phichit.jpg",
+  "กำแพงเพชร": "/imgnorth/phichit/Phichit-.jpg",
+  "สุโขทัย": "/imgnorth/phichit/Phichi-t.jpg",
 
   "อยุธยา": "/imagezone/ayutthaya.jpg",
   "นครปฐม": "/imagezone/nakhon-pathom.jpg",
