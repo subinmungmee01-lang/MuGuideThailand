76d531dcb9ca2a57fe75bdd2df65f1e5413ba3d3
Subin-mungmee <betapower00002@gmail.com>
2026-06-01 15:50:08 +0700

    1

--- data/lotteryHistory.ts
+++ data/lotteryHistory.ts
@@ -7,6 +7,7 @@ export interface LotteryDraw {
 }
 
 export const lotteryHistory: LotteryDraw[] = [
+  { date: "1 มิ.ย. 2569", threeFront: "770", threeBack: "95" },
   { date: "16 พ.ค. 2569", threeFront: "387", threeBack: "08" },
   { date: "2 พ.ค. 2569", threeFront: "077", threeBack: "43" },
   { date: "16 เม.ย. 2569", threeFront: "612", threeBack: "77" },
